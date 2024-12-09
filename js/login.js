5aa98b03e5eeb5fb82ef945b3f33e62a1dd2a5f2
AvocadoHead <55915461+AvocadoHead@users.noreply.github.com>
2024-12-09 21:28:45 +0200

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,5 @@
 import { getCurrentLang, setCurrentLang } from './utils.js';
+import { API_BASE_URL } from './api-service.js';
 
 let currentLang = getCurrentLang();
 
@@ -30,7 +31,7 @@ async function handleLogin(event) {
     const errorDiv = document.getElementById('login-error');
     
     try {
-        const response = await fetch('http://localhost:3000/api/auth/login', {
+        const response = await fetch(`${API_BASE_URL}/auth/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
